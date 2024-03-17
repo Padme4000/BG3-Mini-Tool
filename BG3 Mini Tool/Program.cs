0c01bef6e283ce1ddcb347cc82399efbf0e6430e
German Fernandez <german.fernandez@fusionit.uy>
2024-03-17 14:19:58 -0300

    Updated Initialization parameters to proper scaling DPI on high resolution

--- BG3 Mini Tool/Program.cs
+++ BG3 Mini Tool/Program.cs
@@ -10,7 +10,13 @@ namespace BG3_Mini_Tool
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+
+            // Instead of using the default font and display settings,
+            // we set the high DPI mode to DpiUnaware to allow the application to scale properly.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
+            
             Application.Run(new Form1());
         }
     }
